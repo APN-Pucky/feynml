89e70a0603f12f57f9c1c32be5c9f7c34795de9b
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2023-07-23 22:20:41 +0200

    expose more

--- feynml/__init__.py
+++ feynml/__init__.py
@@ -8,8 +8,14 @@ package = "feynml"
 
 __version__ = version(package)
 
+from .connector import Connector
 from .feynmandiagram import FeynmanDiagram
+from .feynml import feynml
+from .head import Head
 from .leg import Leg
+from .meta import Meta
 from .pdgid import PDG
+from .point import Point
 from .propagator import Propagator
+from .styled import Styled
 from .vertex import Vertex
